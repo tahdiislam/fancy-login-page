48f20c3f7cfd1334c11d090dc44f3f673e1d5d21
tahdi-islam <tshdi60@gmail.com>
2022-10-15 18:22:10 +0600

    add google and github btn

--- src/Comonent/RegisterPage/RegisterPage.jsx
+++ src/Comonent/RegisterPage/RegisterPage.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const RegisterPage = () => {
+    // form submit
+    const handleFormSubmit = e => {
+        e.preventDefault()
+    }
     return (
         <div>
             <h1 className="text-3xl font-semibold my-3 text-emerald-400">Please Register</h1>
@@ -9,11 +13,8 @@ const RegisterPage = () => {
             <section className="p-6 dark:bg-gray-800 dark:text-gray-100">
                 <div className="container grid gap-6 mx-auto text-center lg:grid-cols-2 xl:grid-cols-5">
                     <div className="w-full px-6 py-16 rounded-md sm:px-12 md:px-16 xl:col-span-2 dark:bg-gray-900">
-                        <h1 className="text-5xl font-extrabold dark:text-gray-50">Submit the form</h1>
-                        <p className="my-8">
-                            <span className="font-medium dark:text-gray-50">Modular and versatile.</span>Fugit vero facilis dolor sit neque cupiditate minus esse accusamus cumque at.
-                        </p>
-                        <form novalidate="" action="" className="self-stretch space-y-3 ng-untouched ng-pristine ng-valid">
+                        <h1 className="text-5xl font-extrabold dark:text-gray-50 my-6">Submit the form</h1>
+                        <form onSubmit={handleFormSubmit} action="" className="self-stretch space-y-3 ng-untouched ng-pristine ng-valid">
                             <div>
                                 <label for="name" className="text-sm sr-only">Your name</label>
                                 <input id="name" type="text" placeholder="Your name" className="w-full rounded-md focus:ring focus:ring-emerald-400 dark:border-gray-700 p-2" />
@@ -26,7 +27,12 @@ const RegisterPage = () => {
                                 <label for="password" className="text-sm sr-only">Password</label>
                                 <input id="password" type="password" placeholder="Password" className="w-full rounded-md focus:ring focus:ring-emerald-400 dark:border-gray-700 p-2" />
                             </div>
-                            <button type="submit" className="w-full py-2 font-semibold rounded dark:bg-emerald-400 dark:text-gray-900">Register</button>
+                            <button type="submit" className="w-full py-2 font-semibold rounded dark:bg-emerald-400 dark:text-gray-900 border-emerald-50 
+                            focus:bg-emerald-500 focus:text-gray-600">Register</button>
+                            <button type="submit" className="w-full py-2 font-semibold rounded dark:bg-emerald-400 dark:text-gray-900 border-emerald-50 
+                            focus:bg-emerald-500 focus:text-gray-600 flex justify-center"><img className='h-7' src="https://cdn-icons-png.flaticon.com/512/2504/2504739.png" alt="" /></button>
+                            <button type="submit" className="w-full py-2 font-semibold rounded dark:bg-emerald-400 dark:text-gray-900 border-emerald-50 
+                            focus:bg-emerald-500 focus:text-gray-600 flex justify-center"><img className='h-7' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="logo img" /></button>
                             <p><small>Log in <Link to='/login' className='text-emerald-400 underline cursor-pointer'>here</Link></small></p>
                         </form>
                     </div>
