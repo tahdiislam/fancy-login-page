3ae64c9f70fc4c458e07b50b95b069437baceb9c
tahdi-islam <tshdi60@gmail.com>
2022-10-15 15:13:11 +0600

    m

---
+++ src/Comonent/Layout/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+          <h1 className="text-4xl font-semi my-4">Fancy-Form</h1>  
+        </div>
+    );
+};
+
+export default Main;
