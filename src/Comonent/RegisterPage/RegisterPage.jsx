dc9ec8310442d0c006e218f677641aaa5c2b9295
tahdi-islam <tshdi60@gmail.com>
2022-10-15 21:19:24 +0600

    add github login system

--- src/Comonent/RegisterPage/RegisterPage.jsx
+++ src/Comonent/RegisterPage/RegisterPage.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import app from '../../firebase/firebase.init';
 
 const auth = getAuth(app)
 const googleProvider = new GoogleAuthProvider()
+const githubProvider = new GithubAuthProvider()
 
 
 const RegisterPage = () => {
@@ -23,6 +24,21 @@ const RegisterPage = () => {
          .catch(error => {
             setError(error.message)
             console.error(error);
+         })
+    }
+    // github sign in
+    const handleGithubSignIn = () => {
+        setSuccess(false)
+        setError('')
+        signInWithPopup(auth, githubProvider)
+         .then(result => {
+            const user = result.user;
+            setSuccess(true)
+            console.log(user);
+         })
+         .catch(error => {
+            setError(error.message)
+            console.log(error);
          })
     }
     // form submit
@@ -54,14 +70,14 @@ const RegisterPage = () => {
                             focus:bg-emerald-500 focus:text-gray-600">Register</button>
                             <button onClick={handleGoogleSignIn} type="submit" className="w-full py-2 font-semibold rounded dark:bg-emerald-400 dark:text-gray-900 border-emerald-50 
                             focus:bg-emerald-500 focus:text-gray-600 flex justify-center"><img className='h-7' src="https://cdn-icons-png.flaticon.com/512/2504/2504739.png" alt="" /></button>
-                            <button type="submit" className="w-full py-2 font-semibold rounded dark:bg-emerald-400 dark:text-gray-900 border-emerald-50 
+                            <button onClick={handleGithubSignIn} type="submit" className="w-full py-2 font-semibold rounded dark:bg-emerald-400 dark:text-gray-900 border-emerald-50 
                             focus:bg-emerald-500 focus:text-gray-600 flex justify-center"><img className='h-7' src='https://cdn-icons-png.flaticon.com/512/25/25231.png' alt="logo img" /></button>
                             <p><small>Log in <Link to='/login' className='text-emerald-400 underline cursor-pointer'>here</Link></small></p>
                             <span>{success && <small className='text-green-500'>Account created successfully</small>}</span>
-                            <span>{error && <small className='text-red-500'>{error}</small>}</span>
+                            <span>{error && <small className='text-red-500'>{error.split('Firebase:').join('')}</small>}</span>
                         </form>
                     </div>
-                    <img src="https://source.unsplash.com/random/480x360" alt="" className="object-cover w-full rounded-md xl:col-span-3 dark:bg-gray-500" />
+                    <img src="https://images.unsplash.com/photo-1665472832769-c8bffec9296f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=360&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY2NTg0NTE5MA&ixlib=rb-1.2.1&q=80&w=480" alt="" className="object-cover w-full rounded-md xl:col-span-3 dark:bg-gray-500" />
                 </div>
             </section>
         </div>
